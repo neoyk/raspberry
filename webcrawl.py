9f17c06146326dff118673147074219d72fb2c54
neoyk <yukun2005@gmail.com>
2015-01-11 10:45:52 +0800

    tweak

--- webcrawl.py
+++ webcrawl.py
@@ -486,7 +486,7 @@ class webperf(threading.Thread):
             webdomain = webdomain.replace("'","\\'")
             webdomain = webdomain.replace('"','\\"')
             if self.wtype.startswith('C'):
-                wgetcmd = 'wget -{0} -vt 1 -T 10 -o {1} -O /dev/null --header="Host:{2}" {3}/{4}'.format(self.version, logpath, domain[0], self.wip, domain[1])
+                egetcmd = 'wget -{0} -vt 1 -T 10 -o {1} -O /dev/null --header="Host:{2}" {3}/{4}'.format(self.version, logpath, domain[0], self.wip, domain[1])
             else:
                 wgetcmd = 'wget -'+str(self.version)+' -vt 1 -T 10 -o '+logpath+' -O /dev/null '+webdomain
             #wgetcmd = 'wget -'+str(self.version)+' -vt 1 -T 10 -U "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1; 360Spider" -o '+logpath+' -O /dev/null '+webdomain
@@ -541,11 +541,11 @@ class webperf(threading.Thread):
                 webdomain = m[-1]
                 if(webdomain.count('/')>1 and webdomain[-1]=='/'):
                     webdomain = webdomain[:-1]
-                try:
-                    self.cur1.execute("update ipv"+str(self.version)+"server set webdomain=%s where id = %s",( webdomain, webid ))
-                except:
-                    pass
-                self.logger.info( "HTTP redirection found. Changing webdomain to "+webdomain)
+                #try:
+                #    self.cur1.execute("update ipv"+str(self.version)+"server set webdomain=%s where id = %s",( webdomain, webid ))
+                #except:
+                #    pass
+                self.logger.info( "HTTP redirection found. Real webdomain: "+webdomain)
             ip, asn = self.wgetip(log)
             #if self.wtype.startswith('C') and ip!='0.0.0.0' and ip != self.wip:
             if self.wtype.startswith('C') and ip != self.wip:
